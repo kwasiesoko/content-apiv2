78d031200086672112d881656b765cae6b88a4dc
kwasiesoko <kwasi@esoko.com>
2026-05-11 17:37:43 +0000

    aseye

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors();
+  app.enableCors("*");
   await app.listen(process.env.PORT ?? 3000);
 }
 
